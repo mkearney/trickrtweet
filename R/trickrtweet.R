60641115d483d74387bca2b23b880d5c1031c4d5
Michael Kearney <mkearney@ku.edu>
2017-07-24 17:08:17 -0500

    unfollowback and package loading funs

--- R/trickrtweet.R
+++ R/trickrtweet.R
@@ -4,10 +4,13 @@
 #'
 #' @docType package
 #' @name trickrtweet
+#' @import rtweet
 NULL
 
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("Ready to build your Twitter network!")
+  home_user()
+  validate_token()
 }
 
 .trickrtweet <- new.env()
@@ -33,6 +36,7 @@ home_user.default <- function() {
   ## remove at sign
   user <- gsub("@", "", user)
   ## save as environment variable
+  message("Saving your Twitter screen name as environment variable")
   cat(
     paste0("TWITTER_SCREEN_NAME=", user),
     fill = TRUE,
@@ -42,6 +46,16 @@ home_user.default <- function() {
   ## store in pkg environment
   assign(".user", user, envir = .trickrtweet)
   ## return screen name
-  user
+  invisible(user)
 }
 
+#' @importFrom rtweet get_tokens
+validate_token <- function() {
+  token <- rtweet::get_tokens()
+  if (identical(home_user(), token$credentials$screen_name)) {
+    return(invisible(TRUE))
+  }
+  message("Invalid token. Please save path to token associated with home user's account")
+  message("as the \"TWITTER_PAT\" environment variable.")
+  invisible(FALSE)
+}
--- R/unfollowback.R
+++ R/unfollowback.R
@@ -30,14 +30,12 @@ unfollowback <- function(keep = NULL, ...) {
 #' @importFrom rtweet get_friends get_followers lookup_users
 #' @export
 unfollowback.default <- function(keep = NULL) {
-  if (is.null(user)) {
-    user <- home_user()
-  }
+  user <- home_user()
   ## friends
-  fds <- get_friends(user)
+  fds <- rtweet::get_friends(user)
   fds <- fds$user_id
   ## followers
-  flw <- get_followers(user)
+  flw <- rtweet::get_followers(user)
   flw <- flw$user_id
   ## id people who don't follow back
   tounf <- fds[!fds %in% flw]
@@ -45,25 +43,34 @@ unfollowback.default <- function(keep = NULL) {
     message("No haters. Nothing but follow backs!")
     return(invisible())
   }
-  tu_users <- lookup_users(tounf)
+  tu_users <- rtweet::lookup_users(tounf)
   ## keepers
-  if (file.exists(file.path("..", "data", "keepers.rds"))) {
-    keep <- c(readRDS("../data/keepers.rds"), keep)
+  keepers <- Sys.getenv("TWITTER_KEEPERS")
+  if (file.exists(keepers) && !identical("", keepers)) {
+    keepers <- readRDS(keepers)
+    keep <- c(keepers, keep)
   }
   if (!is.null(keep)) {
-    kprs_data <- lookup_users(keep)
+    kprs_data <- rtweet::lookup_users(keep)
     tounf <- tu_users$user_id[!tu_users$user_id %in% kprs_data$user_id]
   }
   ## final vector to unfollow
   tounf <- fds[fds %in% tounf]
+  ## if none then say so
+  if (length(tounf) == 0L) {
+    message("No one left to unfollow!")
+    return(invisible())
+  }
   ## unfollow
   out <- Map(
-    tounf, user = unF, token = token, n = runif(1, 1, 5)
+    unfollow_user, user = tounf
   )
+  ## return invisible
+  return(out)
 }
 
 #' @importFrom rtweet post_unfollow_user
-unF <- function(..., sleeper = TRUE) {
+unfollow_user <- function(..., sleeper = TRUE) {
   if (sleeper) {
     ## sleep an average of 2 seconds per call
     Sys.sleep(runif(1, .5, 3))
