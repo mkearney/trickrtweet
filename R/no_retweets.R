8e125b4b438e1ff8906f75b795282c86cb09c41c
Kearney <kearneymw@mu-132181.dhcp.missouri.edu>
2017-08-28 12:48:52 -0500

    fixed no retweets internals

--- R/no_retweets.R
+++ R/no_retweets.R
@@ -11,25 +11,27 @@
 #' @importFrom rtweet post_follow
 #' @importFrom httr warn_for_status
 no_retweets <- function(n = 1000L, refresh = TRUE) {
-  UseMethod("no_retweets")
+  do.call("no_retweets_", list(n, refresh))
 }
 
-#' no_retweets
+#' no_retweets_
 #'
 #' Mutes retweets from most recent n users
 #' 
 #' @param n Number of most recent users to mute retweets.
 #' @param refresh Logical indicating whether to refresh friends list.
 #' @return API response object.
-#' @export
 #' @importFrom rtweet post_follow
 #' @importFrom httr warn_for_status
-no_retweets.default <- function(n = 1000L, refresh = TRUE) {
-  if (refresh) {
+no_retweets_ <- function(n, refresh) {
+  user <- home_user()
+  if (refresh || !".fds" %in% ls(envir = .trickrtweet, all.names = TRUE)) {
     fds <- as.character(rtweet::get_friends(user)[["user_id"]])
     assign(".fds", fds, envir = .trickrtweet)
+  } else {
+    fds <- get(".fds", envir = .trickrtweet)
   }
-  fds <- get(".fds", envir = .trickrtweet)
+  ## modify to follow without retweets
   f <- function(x) {
     for (i in seq_along(x)) {
       r <- rtweet::post_follow(x[i], retweets = FALSE)
